dabd267a71f18e7c3af75fc1f93f2b786eba3d6a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-14 11:54:42 +0530

    Verified that each module is wrapped in IIFE with console.log(arguments)

---
+++ test-module-1.js
@@ -0,0 +1,21 @@
+class Calculator {
+  constructor() {}
+
+  add(a, b) {
+    return a + b
+  }
+
+  sub(a, b) {
+    return a - b
+  }
+
+  multiply(a, b) {
+    return a * b
+  }
+
+  divide(a, b) {
+    return a / b
+  }
+}
+
+module.exports = Calculator
