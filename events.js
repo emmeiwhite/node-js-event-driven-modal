ffa77ec3b86e5e61e5d3c2a8bbac131cbfd186a0
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-14 07:58:30 +0530

    Same Event -> Multiple Handlers

--- events.js
+++ events.js
@@ -6,4 +6,8 @@ myEmitter.on('newSale', () => {
   console.log('There was a new sale')
 })
 
+myEmitter.on('newSale', () => {
+  console.log('Customer is Imran')
+})
+
 myEmitter.emit('newSale') // Registering or setting up an event
