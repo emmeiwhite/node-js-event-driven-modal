28d0ff5a324a59a61c7ef6ab8f428ef77e16f7f9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-14 12:13:02 +0530

    Named import pattern in node

--- modules.js
+++ modules.js
@@ -7,3 +7,14 @@ console.log(require('module').wrapper)
 const Calculator = require('./test-module-1')
 const c = new Calculator()
 c.add(34, 45)
+
+// exports
+/* --
+const exportsObj = require('./test-module-2')
+console.log(exportsObj)
+console.log(exportsObj.add(12, 24))
+-- */
+
+// Named import pattern in node
+const { add, sub, mul } = require('./test-module-2')
+console.log(mul(10, 20))
